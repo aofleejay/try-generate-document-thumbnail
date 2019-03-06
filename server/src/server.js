7781149a3df7944eca878629f7d999dbf87108e4
aofleejay <aofleejay@hotmail.com>
2019-03-06 17:38:46 +0700

    :sparkles: Create thumbnail image when upload pdf file.

---
+++ server/src/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import gm from 'gm'
+import fileUpload from 'express-fileupload'
+
+const port = process.env.nodePort || 4000
+const app = express()
+gm.subClass({ imageMagick: true })
+
+app.use(fileUpload({
+  limits: { fileSize: 5 * 1024 * 1024 },
+}))
+
+app.post('/files', (req, res) => {
+  gm(req.files.document.data)
+    .toBuffer('jpg', (error, buffer) => {
+      if (!error) {
+        res.setHeader('Content-disposition', `inline; filename*=UTF-8''thumbnail.jpg`)
+        res.send(buffer)
+      } else {
+        res.status(422).json({ error })
+      }
+    })
+})
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
+})
