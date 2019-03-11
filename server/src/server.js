7cdb1d3ca493e0ef63951ce08951fe924c801f2c
aofleejay <aofleejay@hotmail.com>
2019-03-11 13:13:03 +0700

    :art: Formatting code.

--- server/src/server.js
+++ server/src/server.js
@@ -1,57 +1,57 @@
-import express from "express";
-import gm from "gm";
-import fileUpload from "express-fileupload";
-import axios from "axios";
+import express from 'express'
+import gm from 'gm'
+import fileUpload from 'express-fileupload'
+import axios from 'axios'
 
-const port = process.env.nodePort || 4000;
-const app = express();
-gm.subClass({ imageMagick: true });
+const port = process.env.nodePort || 4000
+const app = express()
+gm.subClass({ imageMagick: true })
 
 app.use(
   fileUpload({
-    limits: { fileSize: 5 * 1024 * 1024 }
+    limits: { fileSize: 5 * 1024 * 1024 },
   })
-);
+)
 
-app.post("/files", (req, res) => {
+app.post('/files', (req, res) => {
   gm(req.files.document.data)
     .selectFrame(0)
-    .toBuffer("jpg", (error, buffer) => {
+    .toBuffer('jpg', (error, buffer) => {
       if (!error) {
         res.setHeader(
-          "Content-disposition",
+          'Content-disposition',
           `inline; filename*=UTF-8''thumbnail.jpg`
-        );
-        res.send(buffer);
+        )
+        res.send(buffer)
       } else {
-        res.status(422).json({ error });
+        res.status(422).json({ error })
       }
-    });
-});
+    })
+})
 
-app.get("/files", (req, res) => {
-  const { url } = req.query;
+app.get('/files', (req, res) => {
+  const { url } = req.query
   axios
     .get(`https://docs.google.com/viewer?url=${url}&embedded=true`)
     .then(response => {
-      const imageId = response.data.split("img?id\\u003d")[1].split('",')[0];
+      const imageId = response.data.split('img?id\\u003d')[1].split('",')[0]
       return axios.get(
         `https://docs.google.com/viewerng/img?id=${imageId}&page=0&w=800&webp=true`,
         {
-          responseType: "arraybuffer"
+          responseType: 'arraybuffer',
         }
-      );
+      )
     })
     .then(response => {
-      res.setHeader("Content-Type", "image/png");
-      res.send(Buffer.from(response.data, "binary"));
+      res.setHeader('Content-Type', 'image/png')
+      res.send(Buffer.from(response.data, 'binary'))
     })
     .catch(error => {
-      console.log(error);
-      res.status(422).json({ message: "Can't generate image." });
-    });
-});
+      console.log(error)
+      res.status(422).json({ message: "Can't generate image." })
+    })
+})
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-});
+  console.log(`http://localhost:${port}`)
+})
